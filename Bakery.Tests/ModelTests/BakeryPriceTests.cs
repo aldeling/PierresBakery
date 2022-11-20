bd86125c5413ce5da9a352b62240f73e8ec1c470
Athea DeLing <almdeling@gmail.com>
2022-11-20 12:33:14 -0500

    test 2 fail

--- Bakery.Tests/ModelTests/BakeryPriceTests.cs
+++ Bakery.Tests/ModelTests/BakeryPriceTests.cs
@@ -16,4 +16,15 @@ namespace BakeryPrice.Tests
     }
   }
   
+  [TestClass]
+  public class PastryTests
+  {
+
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
 }
