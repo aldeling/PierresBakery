3382c78203cdca83f7507ca80c85801d412e8b88
Athea DeLing <almdeling@gmail.com>
2022-11-20 13:19:04 -0500

    add inital command line info to program

---
+++ Bakery/program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using BakeryPrice.Models;
+
+namespace BakeryPage
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery");
+      Console.WriteLine("----------------------");
+      Console.WriteLine("Bread Cost: $5 a loaf");
+      Console.WriteLine("Pastries Cost: $2 a pastry");
+      Console.WriteLine("----------------------");
+      Console.WriteLine("Bread Deal: Buy 2, get 1 Free");
+      Console.WriteLine("Pastry Deal: Buy 1 for $2 or 3 for $5");
+      Console.WriteLine("----------------------");
+      Console.WriteLine("Amount of Bread: ");
+      string breadAmount = Console.ReadLine();
+      Console.WriteLine("Amount of Pastries: ");
+      string pastryAmount = Console.ReadLine();
+    }
+  }
+}
